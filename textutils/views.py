1c1f4c2df8ebcf70ffa117e6c41c973931b15e37
nivet79 <32571176+nivet79@users.noreply.github.com>
2022-04-13 17:23:34 +0530

    Update views.py

--- textutils/views.py
+++ textutils/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<H1>Hello world, kakrot</H1>")
+    return HttpResponse("<H1>Hello world, kakrot, testing</H1>")
